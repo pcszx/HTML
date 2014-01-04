9ed0c3b59d7d46d94cf12ef5f23df52144b8b5a8
Dun <pcszx.shen@gmail.com>
2014-01-04 23:55:45 +0100

    projet_web
    iweb+html

---
+++ shen/website/cv_files/cv.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.2 local-build-20140104
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('cv_files/cvMoz.css')
+Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ shen/website/ressources_files/ressources.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20140104
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ressources_files/ressourcesMoz.css')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
